9988c55456cfa3fae95c1397051949ebcb12d15f
Chris Russell <40370458+ninehundreds@users.noreply.github.com>
2022-02-03 13:15:31 -0500

    Added iOS and Android client demos of network services.

--- androidApp/src/main/java/com/ninehundreds/kmmworks/android/MainActivity.kt
+++ androidApp/src/main/java/com/ninehundreds/kmmworks/android/MainActivity.kt
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ninehundreds.kmmworks.Greeting
 import android.widget.TextView
+import com.ninehundreds.kmmworks.network.WhizApiClient
+import com.ninehundreds.kmmworks.network.WhizBang
+import kotlinx.coroutines.runBlocking
 
 fun greet(): String {
     return Greeting().greet()
 }
 
 class MainActivity : AppCompatActivity() {
+
+    private val apiClient = WhizApiClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,4 +23,61 @@ class MainActivity : AppCompatActivity() {
         val tv: TextView = findViewById(R.id.text_view)
         tv.text = greet()
     }
+
+    private fun getAllWhizBangs() {
+
+        runBlocking {
+            apiClient.getWhizBangs(success = {
+
+            }, failure = {
+                it?.printStackTrace()
+            })
+        }
+    }
+
+    private fun getWhizBang(id: String) {
+
+        runBlocking {
+            apiClient.getWhizBang(id, success = {
+
+            }, failure = {
+                it?.printStackTrace()
+            })
+        }
+    }
+
+    private fun deleteWhizBang(id: String) {
+
+        runBlocking {
+            apiClient.deleteWhizBang(id, success = {
+
+            }, failure = {
+                it?.printStackTrace()
+            })
+        }
+    }
+
+    private fun createWhizBang(code: Int, snap: String, desc: String) {
+
+        runBlocking {
+            apiClient.createWhizBang(code, snap, desc,  success = {
+
+            }, failure = {
+                it?.printStackTrace()
+            })
+        }
+
+    }
+
+    private fun updateWhizBang(whizBang: WhizBang) {
+
+        runBlocking {
+            apiClient.updateWhizBang(whizBang, success = {
+
+            }, failure = {
+                it?.printStackTrace()
+            })
+        }
+    }
+
 }
--- iosApp/iosApp/ViewController.swift
+++ iosApp/iosApp/ViewController.swift
@@ -12,13 +12,71 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
 
+    let apiClient = WhizApiClient()
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         titleLabel.text = Greeting().greet()
+    }
+
+    private func getAllWhizBangs() {
+
+        Task {
+           try await apiClient.getWhizBangs(success: { whizBangs in
+               print("")
+           }, failure: { error in
+               print(error?.description ?? "unknown error")
+           })
+       }
 
     }
 
+    private func getWhizBang(id: String) {
+
+        Task {
+            try await apiClient.getWhizBang(id: id, success: { whizBang in
+
+            }, failure: { error in
+                print(error?.description ?? "unknown error")
+            })
+        }
+    }
+
+    private func deleteWhizBang(id: String) {
+        Task {
+            try await apiClient.deleteWhizBang(id: id, success: { response in
+
+            }, failure: { error in
+                print(error?.description ?? "unknown error")
+            })
+        }
+    }
+
+    private func createWhizBang(code: Int, snap: String, desc: String) {
+
+        Task {
+            try await apiClient.createWhizBang(code: Int32(code), snap: snap, desc: desc, success: { whizBang in
+
+           }, failure: { error in
+               print(error?.description ?? "unknown error")
+           })
+       }
+
+    }
+
+    private func updateWhizBang(whizBang: WhizBang) {
+
+        Task {
+            try await apiClient.updateWhizBang(whizBang: whizBang, success: { whizBang in
+
+           }, failure: { error in
+               print(error?.description ?? "unknown error")
+           })
+       }
+
+    }
 
 }
 
